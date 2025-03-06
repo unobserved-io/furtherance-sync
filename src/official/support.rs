e27f7f8dccab34dcd7a474577fac1a5adcad8a84
Ricky Kresslein <ricky@unobserved.io>
2025-03-06 13:17:42 +0100

    Update copyright to 2025

--- src/official/support.rs
+++ src/official/support.rs
@@ -1,5 +1,5 @@
 // Furtherance Sync
-// Copyright (C) 2024  Ricky Kresslein <rk@unobserved.io>
+// Copyright (C) 2025  Ricky Kresslein <rk@unobserved.io>
 //
 // This code is licensed under the Elastic License 2.0.
 // For details: https://www.elastic.co/licensing/elastic-license
